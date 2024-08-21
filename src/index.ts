b71e2358e26b1624ab90c7a0c2c579ccb40f9819
Doarakko <2wua4nlyi@gmail.com>
2024-08-22 02:51:54 +0900

    change endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { SlackApp, SlackEdgeAppEnv } from "slack-cloudflare-workers";
+import { SlackApp, SlackEdgeAppEnv } from 'slack-cloudflare-workers'
 
 export default {
   async fetch(
@@ -6,22 +6,34 @@ export default {
     env: SlackEdgeAppEnv,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const app = new SlackApp({ env });
+    const app = new SlackApp({ env })
 
-    app.event("app_mention", async ({ context }) => {
-        const response = await fetch("https://db.ygoprodeck.com/api/v7/randomcard.php", {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          });
-        const data = await response.json();
-        const cardName = data["name"];        
-        const imageUrl = data["card_images"][0].image_url;
+    app.event('app_mention', async ({ context }) => {
+      const response = await fetch(
+        'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=1&offset=0&sort=random&cachebust',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      if (!response.ok) {
+        throw new Error('failed to fetch card data')
+      }
 
-        await context.say({text: `<${imageUrl}|${cardName}>`});
-    });
-    
-    return await app.run(request, ctx);
-  },
-};
+      const data: {
+        data: { name: string; card_images: { image_url: string }[] }[]
+      } = await response.json()
+      if (!data.data[0]) {
+        throw new Error('failed to get card data')
+      }
+
+      const cardName = data.data[0].name
+      const imageUrl = data.data[0].card_images[0].image_url
+      await context.say({ text: `<${imageUrl}|${cardName}>` })
+    })
+
+    return await app.run(request, ctx)
+  }
+}
